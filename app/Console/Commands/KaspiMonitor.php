0ba1e5934eb248f1e54686f4d27fb3ceb8dd9de9
RuslanShigabutdinov <SaylerPlay@gmail.com>
2025-10-07 09:12:35 +0500

    git config test

--- app/Console/Commands/KaspiMonitor.php
+++ app/Console/Commands/KaspiMonitor.php
@@ -10,18 +10,8 @@ use App\Services\GoogleSpreadsheetService;
 
 class KaspiMonitor extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'app:kaspi-monitor';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'Check each KaspiProduct entry to find all products that have lower prices';
 
     public function handle(
